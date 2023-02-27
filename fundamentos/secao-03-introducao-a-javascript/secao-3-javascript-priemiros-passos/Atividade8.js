c150d7dcab0e4a47bf840639a67efc002c7fb686
Pedro Gabriel <pglfreitas@gmail.com>
2023-02-27 16:27:14 -0300

    Conclusão dos exercicios 7,8 e 9 de 1-3.1

---
+++ fundamentos/secao-03-introducao-a-javascript/secao-3-javascript-priemiros-passos/Atividade8.js
@@ -0,0 +1,10 @@
+let a = 1
+let b = 2
+
+if(a > b) {
+    console.log("1 é maior que 2")
+}
+
+else {
+    console.log("2 é maior que 1")
+}
---
+++ fundamentos/secao-03-introducao-a-javascript/secao-3-javascript-priemiros-passos/Atividade9.js
@@ -0,0 +1,16 @@
+let a = 1
+let b = 2
+let c = 3
+
+if (a > b && a > c){
+    console.log("1 é maior que 2 e 3")
+}
+else if (b > a && b > c){
+    console.log("2 é maior que 1 e 3")
+}
+
+else if (c > a && c > b){
+    console.log("3 é maior que 1 e 2")
+}
+
+
