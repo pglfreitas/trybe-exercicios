df24275271b52a80a130007d1742c7179fa4d8cd
Pedro Gabriel <pglfreitas@gmail.com>
2023-02-27 16:14:04 -0300

    Conclusão da atividade 6 dia 1-3.1

---
+++ fundamentos/secao-03-introducao-a-javascript/secao-3-javascript-priemiros-passos/Atividade6.js
@@ -0,0 +1,17 @@
+let resultado ="aprovada";
+
+switch (resultado) { 
+    case "aprovada":
+    console.log("Parabéns, você está no grupo de pessoas aprovadas")
+    break
+
+    case "lista":
+    console.log("Vocẽ está na nossa lista de espera.")
+    break
+    case "reprovada":
+    console.log("Infelizmente, vocẽ reprovou.")
+    break
+    
+    default:
+    console.log("Informação incorreta.")
+}
