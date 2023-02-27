9ecdeea6d9dcbbfccc54e9ba706cca70caea3a5b
Pedro Gabriel <pglfreitas@gmail.com>
2023-02-27 16:00:45 -0300

    Conclusão exercicio 5 1-3.1

---
+++ fundamentos/secao-03-introducao-a-javascript/secao-3-javascript-priemiros-passos/atividade5.js
@@ -0,0 +1,9 @@
+let weekDay = "quarta-feira"
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira" ) {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+
+else {
+    console("FINALMENTE, descanso merecido! Uwu!")
+}
