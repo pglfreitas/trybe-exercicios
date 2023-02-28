eb2d440a13b04417592900fba87810ef373bc486
Pedro Gabriel <pglfreitas@gmail.com>
2023-02-28 16:29:41 -0300

    Conclusão do exercicio 6 e 7 dia 2 seção 3

--- fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/Atividade6.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/Atividade6.js
@@ -19,3 +19,36 @@ if(soma > 20) {
 else {
     console.log("Valor menor ou igual a 20")
 }
+
+let maiorNumero = numbers[0]
+
+for(let index2 = 1; index2 < numbers.length; index2+= 1){
+    if (numbers[index2] > maiorNumero) {maiorNumero = numbers[index2]}
+}
+
+console.log(maiorNumero)
+
+let numerosImpares = 0
+
+for(let index3 = 1; index3 < numbers.length; index3+= 1) {
+    if (numbers[index3] % 2 !== 0) {
+        numerosImpares += 1
+    }
+}
+console.log(numerosImpares)
+
+if (numerosImpares === 0) {
+    console.log("Nenhum valor ímpar encontrado")
+}
+
+let menorNumero = numbers[0]
+
+for(let index4 = 1; index4 < numbers.length; index4+= 1){
+    if (numbers[index4] < menorNumero) {
+        menorNumero = numbers[index4]
+    }
+}
+
+console.log(menorNumero)
+
+
